e1c5ff2d2cf82eab9a6f56989dffe57829d89d43
Moises Ariza <arizamoises@gmail.com>
2022-04-16 18:38:17 -0500

    Add form and customization with tailwind

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import ilustration from  '../imgs/prueba.svg'
+import Form from './contact.form'
 
 const Header = () => {
     return(
-        <header className='bg-purple-300'>
-            <div className='container mx-auto p-12 max-w-4xl flex justify-center items-center'>
-                <div className='flex-1'>
-                    <h1>Hola Soy Moises Ariza</h1>
-                    <p>Formador en desarrollo web fullstack y desarrollador backend</p>
+        <header className='bg-gray-300'>
+            <div className='container mx-auto p-12 max-w-4xl'>
+                <div className='flex justify-center items-center'>
+                    <div className='flex-1'>
+                        <h1 className=' font-bold text-purple-700 text-6xl'>Hola Soy Moises Ariza</h1>
+                        <p className='text-xl'>Formador en desarrollo web fullstack y desarrollador backend</p>
+                    </div>
+                    
+                    <img src={ilustration} alt="Hombre futurista" style={{height: "300px"}}/>
                 </div>
-                
-                <img src="" alt="" />
+                <div>
+                    <Form/>
+                </div>
             </div>
-            
         </header>
     )
     
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Header from '../components/header'
 export default function Home() {
   return (
     <>
-      <div>Probado</div>
       <Header/>
     </>
   
