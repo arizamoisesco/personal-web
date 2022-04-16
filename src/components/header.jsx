db7d100f32f48507b7253e361c01350b5f91855c
Moises Ariza <arizamoises@gmail.com>
2022-04-16 14:22:40 -0500

    Add Header Component

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className='bg-purple-300'>
+            <div className='container mx-auto p-12 max-w-4xl flex justify-center items-center'>
+                <div className='flex-1'>
+                    <h1>Hola Soy Moises Ariza</h1>
+                    <p>Formador en desarrollo web fullstack y desarrollador backend</p>
+                </div>
+                
+                <img src="" alt="" />
+            </div>
+            
+        </header>
+    )
+    
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from '../components/header'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <div>Probado</div>
+      <Header/>
+    </>
+  
+  );
 }
