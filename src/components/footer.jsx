b4b63e71aeff07f8712cd2886bcf1fe335036217
Moises Ariza <arizamoises@gmail.com>
2022-04-27 22:55:24 -0500

    Add footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <footer className='bg-blue-100 mt-16 text-center font-bold grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 my-4"'> 
+            <section>
+                <p>Hecho con amor</p>
+                <p> 2022 </p> 
+            </section>
+            <section>
+                <p>Twitter</p>
+                <p>Linkedin</p>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Footer from "../components/footer";
 import Header from '../components/header'
 import Repos from '../components/repos'
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header/>
       <Repos />
+      <Footer />
     </>
   
   );
